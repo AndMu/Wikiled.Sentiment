70f587a6076eaeb77c2b2b13ab0110e941fb3905
Andrius Mudinas <keistokas@gmail.com>
2017-09-10 18:33:15 +0100

    Fix tests

--- src/Sentiment/Wikiled.Sentiment.Text.Tests/NLP/Style/InquirerFingerPrintTests.cs
+++ src/Sentiment/Wikiled.Sentiment.Text.Tests/NLP/Style/InquirerFingerPrintTests.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Moq;
 using NUnit.Framework;
 using Wikiled.Sentiment.TestLogic.Shared.Helpers;
 using Wikiled.Sentiment.Text.NLP.Style;
-using Wikiled.Text.Inquirer.Data;
 
 namespace Wikiled.Sentiment.Text.Tests.NLP.Style
 {
@@ -17,19 +15,20 @@ namespace Wikiled.Sentiment.Text.Tests.NLP.Style
         {
             var document = await ActualWordsHandler.Instance.Loader.InitDocument().ConfigureAwait(false);
             var block = new TextBlock(ActualWordsHandler.Instance.WordsHandler, document.Sentences.ToArray());
-            Assert.AreEqual(178, block.InquirerFinger.InquirerProbabilities.AllLeafs.Count());
-            Assert.AreEqual("PLACE", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(1).First().Name);
+            Assert.AreEqual(190, block.InquirerFinger.InquirerProbabilities.AllLeafs.Count());
+            int index = 12;
+            Assert.AreEqual("PLACE", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(1 + index).First().Name);
             Assert.AreEqual(0.0, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.First().Value, 2));
-            Assert.AreEqual("Hostile", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(36).First().Name);
-            Assert.AreEqual(0.08, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(35).First().Value, 2));
-            Assert.AreEqual("Exch", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(24).First().Name);
-            Assert.AreEqual(0.0, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(24).First().Value, 2));
-            Assert.AreEqual("Strong", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(34).First().Name);
-            Assert.AreEqual(0.17, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(34).First().Value, 2));
-            Assert.AreEqual(2, block.InquirerFinger.InquirerProbabilities.Branches.Count);
-            Assert.AreEqual(17, block.InquirerFinger.InquirerProbabilities.Branches[0].Branches.Count);
-            Assert.AreEqual("Location", block.InquirerFinger.InquirerProbabilities.Branches[0].Branches[0].Name);
-            Assert.AreEqual(0.02, Math.Round((double)block.InquirerFinger.InquirerProbabilities.Branches[0].Branches[0].Leafs[0].Value, 2));
+            Assert.AreEqual("Hostile", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(36 + index).First().Name);
+            Assert.AreEqual(0.08, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(35 + index).First().Value, 2));
+            Assert.AreEqual("Exch", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(24 + index).First().Name);
+            Assert.AreEqual(0.0, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(24 + index).First().Value, 2));
+            Assert.AreEqual("Strong", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(34 + index).First().Name);
+            Assert.AreEqual(0.17, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(34 + index).First().Value, 2));
+            Assert.AreEqual(3, block.InquirerFinger.InquirerProbabilities.Branches.Count);
+            Assert.AreEqual(17, block.InquirerFinger.InquirerProbabilities.Branches[1].Branches.Count);
+            Assert.AreEqual("Location", block.InquirerFinger.InquirerProbabilities.Branches[1].Branches[0].Name);
+            Assert.AreEqual(0.02, Math.Round((double)block.InquirerFinger.InquirerProbabilities.Branches[1].Branches[0].Leafs[0].Value, 2));
         }
 
         [Test]
@@ -37,19 +36,20 @@ namespace Wikiled.Sentiment.Text.Tests.NLP.Style
         {
             var document = await ActualWordsHandler.Instance.Loader.InitDocument("cv001_19502.txt").ConfigureAwait(false);
             var block = new TextBlock(ActualWordsHandler.Instance.WordsHandler, document.Sentences.ToArray());
-            Assert.AreEqual(178, block.InquirerFinger.InquirerProbabilities.AllLeafs.Count());
-            Assert.AreEqual("PLACE", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(1).First().Name);
+            int index = 12;
+            Assert.AreEqual(190, block.InquirerFinger.InquirerProbabilities.AllLeafs.Count());
+            Assert.AreEqual("PLACE", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(1 + index).First().Name);
             Assert.AreEqual(0.0, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.First().Value, 2));
-            Assert.AreEqual("Hostile", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(36).First().Name);
-            Assert.AreEqual(0.07, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(35).First().Value, 2));
-            Assert.AreEqual("Exch", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(24).First().Name);
-            Assert.AreEqual(0.0, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(24).First().Value, 2));
-            Assert.AreEqual("Strong", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(34).First().Name);
-            Assert.AreEqual(0.17, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(34).First().Value, 2));
-            Assert.AreEqual(2, block.InquirerFinger.InquirerProbabilities.Branches.Count);
-            Assert.AreEqual(17, block.InquirerFinger.InquirerProbabilities.Branches[0].Branches.Count);
-            Assert.AreEqual("Location", block.InquirerFinger.InquirerProbabilities.Branches[0].Branches[0].Name);
-            Assert.AreEqual(0.02, Math.Round((double)block.InquirerFinger.InquirerProbabilities.Branches[0].Branches[0].Leafs[0].Value, 2));
+            Assert.AreEqual("Hostile", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(36 + index).First().Name);
+            Assert.AreEqual(0.07, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(35 + index).First().Value, 2));
+            Assert.AreEqual("Exch", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(24 + index).First().Name);
+            Assert.AreEqual(0.0, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(24 + index).First().Value, 2));
+            Assert.AreEqual("Strong", block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(34 + index).First().Name);
+            Assert.AreEqual(0.17, Math.Round((double)block.InquirerFinger.InquirerProbabilities.AllLeafs.Skip(34 + index).First().Value, 2));
+            Assert.AreEqual(3, block.InquirerFinger.InquirerProbabilities.Branches.Count);
+            Assert.AreEqual(17, block.InquirerFinger.InquirerProbabilities.Branches[1].Branches.Count);
+            Assert.AreEqual("Location", block.InquirerFinger.InquirerProbabilities.Branches[1].Branches[0].Name);
+            Assert.AreEqual(0.02, Math.Round((double)block.InquirerFinger.InquirerProbabilities.Branches[1].Branches[0].Leafs[0].Value, 2));
         }
     }
 }
