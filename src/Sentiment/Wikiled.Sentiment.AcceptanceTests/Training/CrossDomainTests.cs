54c9dac9fb7b8f7018d70b27e6bcb96cef228900
Andrius Mudinas <keistokas@gmail.com>
2017-09-12 00:43:02 +0100

    Test Fixes

--- src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Training/CrossDomainTests.cs
+++ src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Training/CrossDomainTests.cs
@@ -12,7 +12,7 @@ namespace Wikiled.Sentiment.AcceptanceTests.Training
         private static readonly Logger log = LogManager.GetCurrentClassLogger();
 
         [TestCase("B0026127Y8", ProductCategory.Video, 99)]
-        [TestCase("9562910334", ProductCategory.Book, 98)]
+        [TestCase("9562910334", ProductCategory.Book, 99)]
         public async Task TestElectronics(string product, ProductCategory category, int accuracy)
         {
             log.Info("TestElectronics: {0} {1}", product, category);
@@ -20,7 +20,7 @@ namespace Wikiled.Sentiment.AcceptanceTests.Training
             Assert.AreEqual(accuracy, Math.Round(result.Performance.GetSingleAccuracy(true), 2) * 100);
         }
 
-        [TestCase("B0002L5R78", ProductCategory.Electronics, 95)]
+        [TestCase("B0002L5R78", ProductCategory.Electronics, 96)]
         public async Task TestVideo(string product, ProductCategory category, int accuracy)
         {
             log.Info("TestVideo: {0} {1}", product, category);
--- src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Training/SentimentTests.cs
+++ src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Training/SentimentTests.cs
@@ -10,7 +10,7 @@ namespace Wikiled.Sentiment.AcceptanceTests.Training
     {
         private static readonly Logger log = LogManager.GetCurrentClassLogger();
 
-        [TestCase("B00002EQCW", "Total:<213> Positive:<97.47%> Negative:<33.33%> F1:<0.96> RMSE:0.93")]
+        [TestCase("B00002EQCW", "Total:<213> Positive:<97.47%> Negative:<26.67%> F1:<0.96> RMSE:0.94")]
         public async Task TrainedElectronicsSentimentDetection(string product, string performance)
         {
             log.Info("TrainedElectronicsSentimentDetection: {0} {1}", product, performance);
