4843a1d7ccc16c50749748707b45153987ae1aa5
Andrius Mudinas <keistokas@gmail.com>
2017-10-15 21:04:46 +0100

    Update expectations

--- src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Sentiments/Word2VecTests.cs
+++ src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Sentiments/Word2VecTests.cs
@@ -30,7 +30,7 @@ namespace Wikiled.Sentiment.AcceptanceTests.Sentiments
             new SentimentTestData("B009GN6F5Q", 472, 0, "Total:<390> Positive:<85.71%> Negative:<72.65%> F1:<0.87> RMSE:1.26") {Category = ProductCategory.Video},
             new SentimentTestData("B009CG8YJW", 418, 0, "Total:<345> Positive:<69.54%> Negative:<72.09%> F1:<0.80> RMSE:1.35") {Category = ProductCategory.Video},
 
-            new SentimentTestData("B00004SGFS", 381, 0, "Total:<375> Positive:<95.61%> Negative:<72.73%> F1:<0.96> RMSE:0.9") {Category = ProductCategory.Kitchen},
+            new SentimentTestData("B00004SGFS", 381, 0, "Total:<375> Positive:<95.61%> Negative:<72.73%> F1:<0.96> RMSE:0.90") {Category = ProductCategory.Kitchen},
             new SentimentTestData("B000PYF768", 507, 0, "Total:<480> Positive:<90.91%> Negative:<72.50%> F1:<0.94> RMSE:0.94") {Category = ProductCategory.Kitchen},
             new SentimentTestData("B0000Z6JIW", 297, 0, "Total:<288> Positive:<79.55%> Negative:<100.00%> F1:<0.89> RMSE:1.32") {Category = ProductCategory.Kitchen}
         };
