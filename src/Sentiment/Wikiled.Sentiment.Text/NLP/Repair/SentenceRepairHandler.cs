bc5b028d746bda920b6e19fcc5863e255e22342e
Andrius <andrius@wikiled.com>
2018-03-12 12:34:07 +0000

    Upgrade packages

--- src/Sentiment/Wikiled.Sentiment.Text/NLP/Repair/SentenceRepairHandler.cs
+++ src/Sentiment/Wikiled.Sentiment.Text/NLP/Repair/SentenceRepairHandler.cs
@@ -67,7 +67,7 @@ namespace Wikiled.Sentiment.Text.NLP.Repair
                         select item;
             foreach (var item in items)
             {
-                var test = item.Element("Classify");
+                var test = item.Element("Test");
                 if (test == null)
                 {
                     throw new NullReferenceException("test");
