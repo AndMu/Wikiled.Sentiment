6798a309b3b5b338046ae9b15d6602b4ae507493
Andrius Mudinas <andrius@wikiled.com>
2018-06-11 09:59:02 +0100

    Fixing assembly attributes

--- src/Sentiment/Wikiled.Sentiment.Analysis/Properties/AssemblyInfo.cs
+++ src/Sentiment/Wikiled.Sentiment.Analysis/Properties/AssemblyInfo.cs
@@ -1,10 +1,4 @@
-﻿using System.Reflection;
+﻿
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Wikiled.Sentiment.Analysis")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: InternalsVisibleTo("Wikiled.Sentiment.Analysis.Tests")]
--- src/Sentiment/Wikiled.Sentiment.Text/Properties/AssemblyInfo.cs
+++ src/Sentiment/Wikiled.Sentiment.Text/Properties/AssemblyInfo.cs
@@ -1,11 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Wikiled.Sentiment.Text")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: InternalsVisibleTo("Wikiled.Sentiment.Text.Tests")]
 
