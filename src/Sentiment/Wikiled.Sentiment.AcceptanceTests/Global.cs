a51841e71228b042b08d5aacf0d0715c0a5fc2f9
Andrius Mudinas <andrius@wikiled.com>
2018-06-19 12:21:18 +0100

    Making random predictable in tets

---
+++ src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Global.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using Wikiled.MachineLearning.Mathematics;
+
+namespace Wikiled.Sentiment.AcceptanceTests
+{
+    [SetUpFixture]
+    public class Global
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            GlobalSettings.Random = new Random(48);
+        }
+
+        [OneTimeTearDown]
+        public void Clean()
+        {
+        }
+    }
+}
---
+++ src/Sentiment/Wikiled.Sentiment.Integration.Tests/Global.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using Wikiled.MachineLearning.Mathematics;
+
+namespace Wikiled.Sentiment.Integration.Tests
+{
+    [SetUpFixture]
+    public class Global
+    {
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            GlobalSettings.Random = new Random(48);
+        }
+
+        [OneTimeTearDown]
+        public void Clean()
+        {
+        }
+    }
+}
