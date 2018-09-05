9080e4407ac2bb23c505ba5ba3e7d372efd0390e
Andrius Mudinas <keistokas@gmail.com>
2018-09-06 00:08:42 +0100

    Change SVM

--- src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Training/SentimentTests.cs
+++ src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Training/SentimentTests.cs
@@ -10,7 +10,7 @@ namespace Wikiled.Sentiment.AcceptanceTests.Training
     {
         private static readonly Logger log = LogManager.GetCurrentClassLogger();
 
-        [TestCase("B00002EQCW", "Total:<214> Positive:<96.500%> Negative:<71.429%> F1:<0.972> RMSE:0.93")]
+        [TestCase("B00002EQCW", "Total:<214> Positive:<96.000%> Negative:<71.429%> F1:<0.970> RMSE:0.94")]
         [TestCase("B0026127Y8", "Total:<854> Positive:<88.875%> Negative:<39.506%> F1:<0.911> RMSE:1.19")]
         public async Task TestElectronics(string product, string performance)
         {
@@ -20,7 +20,7 @@ namespace Wikiled.Sentiment.AcceptanceTests.Training
         }
 
 
-        [TestCase("B0002L5R78", "Total:<7254> Positive:<89.600%> Negative:<34.014%> F1:<0.909> RMSE:1.50")]
+        [TestCase("B0002L5R78", "Total:<7254> Positive:<89.538%> Negative:<33.878%> F1:<0.909> RMSE:1.29")]
         public async Task TestVideo(string product, string performance)
         {
             log.Info("TestVideo: {0} {1}", product, performance);
--- src/Sentiment/Wikiled.Sentiment.Text/MachineLearning/Classifier.cs
+++ src/Sentiment/Wikiled.Sentiment.Text/MachineLearning/Classifier.cs
@@ -25,9 +25,15 @@ namespace Wikiled.Sentiment.Text.MachineLearning
                                      ParameterRanges =
                                          new GridSearchRangeCollection
                                              {
-                                                 new GridSearchRange("complexity", new[] { 0.001, 0.01, 0.1, 1, 10 }),
+                                                 //new GridSearchRange("complexity", new[] { 0.001, 0.01, 0.1, 1, 10, 100, 1000 }),
+                                                 new GridSearchRange("C", new[] { 0.001, 0.01, 0.1, 1, 10 }),
                                              },
-                                     Learner = p => new LinearDualCoordinateDescent { Complexity = p["complexity"], Loss = Loss.L2 },
+                                     Learner = p => new LinearDualCoordinateDescent
+                                                    {
+                                                        //Complexity = p["complexity"],
+                                                        Loss = Loss.L2,
+                                                        Kernel = new Linear(p["C"])
+                                                    },
                                      Loss = (actual, expected, m) => new ZeroOneLoss(expected).Loss(actual)
             };
 
@@ -35,6 +41,9 @@ namespace Wikiled.Sentiment.Text.MachineLearning
             gridsearch.Token = token;
             var result = gridsearch.Learn(x, y);
             Model = result.BestModel;
+            var parameters = result.BestParameters;
+            var error = result.BestModelError;
+            log.Info("SVM Trained. Threshold: {0} Constant: {1} Error: {2} ...", Model.Threshold, parameters[0].Value, error);
         }
 
         public void Save(string path)
