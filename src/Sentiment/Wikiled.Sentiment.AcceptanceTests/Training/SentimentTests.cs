4ddc89c1d1bb8dcbe64831ffb520d9e2920d95f9
Andrius Mudinas <keistokas@gmail.com>
2018-09-20 21:35:36 +0100

    Update

--- src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Training/SentimentTests.cs
+++ src/Sentiment/Wikiled.Sentiment.AcceptanceTests/Training/SentimentTests.cs
@@ -11,7 +11,7 @@ namespace Wikiled.Sentiment.AcceptanceTests.Training
         private static readonly Logger log = LogManager.GetCurrentClassLogger();
 
         [TestCase("B00002EQCW", "Total:<215> Positive:<98.000%> Negative:<53.333%> F1:<0.973> RMSE:0.89")]
-        [TestCase("B0026127Y8", "Total:<854> Positive:<90.944%> Negative:<43.210%> F1:<0.924> RMSE:1.12")]
+        [TestCase("B0026127Y8", "Total:<854> Positive:<91.074%> Negative:<43.210%> F1:<0.924> RMSE:1.11")]
         public async Task TestElectronics(string product, string performance)
         {
             log.Info("TestElectronics: {0} {1}", product, performance);
@@ -20,7 +20,7 @@ namespace Wikiled.Sentiment.AcceptanceTests.Training
         }
 
 
-        [TestCase("B0002L5R78", "Total:<7275> Positive:<91.386%> Negative:<31.800%> F1:<0.918> RMSE:1.22")]
+        [TestCase("B0002L5R78", "Total:<7275> Positive:<91.723%> Negative:<31.258%> F1:<0.920> RMSE:1.21")]
         public async Task TestVideo(string product, string performance)
         {
             log.Info("TestVideo: {0} {1}", product, performance);
