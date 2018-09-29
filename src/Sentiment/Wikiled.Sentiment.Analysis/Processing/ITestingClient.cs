80b82cd83285ee998cfc254fb7c9c2f0b66617f3
Andrius Mudinas <keistokas@gmail.com>
2018-09-29 19:02:42 +0100

    Exposing property

--- src/Sentiment/Wikiled.Sentiment.Analysis/Processing/ITestingClient.cs
+++ src/Sentiment/Wikiled.Sentiment.Analysis/Processing/ITestingClient.cs
@@ -19,6 +19,8 @@ namespace Wikiled.Sentiment.Analysis.Processing
 
         AspectSentimentTracker AspectSentiment { get; }
 
+        bool UseBuiltInSentiment { get; }
+
         bool DisableAspects { get; set; }
 
         bool DisableSvm { get; set; }
