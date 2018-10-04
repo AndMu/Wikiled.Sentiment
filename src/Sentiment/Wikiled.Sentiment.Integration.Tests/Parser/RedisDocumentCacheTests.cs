90ba7eb16ec7e50ee22b9517a69ccac4a1b600b2
Andrius Mudinas <keistokas@gmail.com>
2018-10-04 21:38:26 +0100

    Fix cache

--- src/Sentiment/Wikiled.Sentiment.Integration.Tests/Parser/RedisDocumentCacheTests.cs
+++ src/Sentiment/Wikiled.Sentiment.Integration.Tests/Parser/RedisDocumentCacheTests.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Memory;
+using Moq;
 using NLog;
 using NUnit.Framework;
 using Wikiled.Redis.Config;
 using Wikiled.Redis.Logic;
 using Wikiled.Sentiment.Text.Cache;
+using Wikiled.Text.Analysis.Cache;
 using Wikiled.Text.Analysis.POS;
 using Wikiled.Text.Analysis.Structure;
 
@@ -21,13 +24,19 @@ namespace Wikiled.Sentiment.Integration.Tests.Parser
 
         private RedisInside.Redis redis;
 
+        private LocalDocumentsCache local;
+
+        private Mock<IMemoryCache> cache;
+
         [SetUp]
         public void Setup()
         {
+            cache = new Mock<IMemoryCache>();
+            local = new LocalDocumentsCache(new MemoryCache(new MemoryCacheOptions()));
             redis = new RedisInside.Redis(i => i.Port(6666).LogTo(item => log.Debug(item)));
             link = new RedisLink("Test", new RedisMultiplexer(new RedisConfiguration("localhost", 6666)));
             link.Open();
-            instance = new RedisDocumentCache(POSTaggerType.Simple, link);
+            instance = new RedisDocumentCache(POSTaggerType.Simple, link, local);
         }
 
         [TearDown]
@@ -45,12 +54,19 @@ namespace Wikiled.Sentiment.Integration.Tests.Parser
             document.Author = "Author";
             document.DocumentTime = new DateTime(2012, 02, 23);
 
-            var result = await instance.GetById(document.Id).ConfigureAwait(false);
+            var result = await instance.GetCached(document).ConfigureAwait(false);
             Assert.IsNull(result);
             await instance.Save(document).ConfigureAwait(false);
-            result = await instance.GetById(document.Id).ConfigureAwait(false);
-            // Maybe that is an issue - we need clonning? Assert.AreNotSame(document, result);
+            result = await instance.GetCached(document).ConfigureAwait(false);
+
+            Assert.AreNotSame(document, result);
             Assert.AreEqual(document.Id, result.Id);
+            Assert.AreEqual(document.Text, result.Text);
+            Assert.AreEqual(document.Author, result.Author);
+
+            result = await instance.GetCached(document).ConfigureAwait(false);
+            document.Id = Guid.NewGuid().ToString();
+            Assert.AreNotEqual(document.Id, result.Id);
             Assert.AreEqual(document.Text, result.Text);
             Assert.AreEqual(document.Author, result.Author);
         }
